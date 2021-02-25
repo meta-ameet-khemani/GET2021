31d8c3b60932d763a36aeda830fa1cfd4efe821f
Ameet Khemani <ameet.khemani@metacube.com>
2021-02-25 19:47:47 +0530

    Assignement 2 question 2 fcfs

---
+++ src/JobScheduler.java
@@ -0,0 +1,126 @@
+/**
+ * JobScheduler class implements First Come First Serve algorithm for job assignment
+ * @author ameet.khemani_metacu
+ *
+ */
+public class JobScheduler {
+
+	/*
+	 * TOTAL_PROCESSES represents total number of processes
+	 */
+	final static int TOTAL_PROCESSES = 6;
+	static int completionTime[] = new int[TOTAL_PROCESSES];
+	static int turnAroundTime[] = new int[TOTAL_PROCESSES];
+	static int waitingTime[] = new int[TOTAL_PROCESSES];
+	static int arrivalAndBurstTiming[][] = new int[TOTAL_PROCESSES][2];
+
+	public static void main(String[] args) {
+		
+		arrivalAndBurstTiming[0][0] = 0;
+		arrivalAndBurstTiming[0][1] = 3;
+		arrivalAndBurstTiming[1][0] = 1;
+		arrivalAndBurstTiming[1][1] = 2;
+		arrivalAndBurstTiming[2][0] = 2;
+		arrivalAndBurstTiming[2][1] = 1;
+		arrivalAndBurstTiming[3][0] = 3;
+		arrivalAndBurstTiming[3][1] = 4;
+		arrivalAndBurstTiming[4][0] = 4;
+		arrivalAndBurstTiming[4][1] = 5;
+		arrivalAndBurstTiming[5][0] = 5;
+		arrivalAndBurstTiming[5][1] = 2;
+		
+	}
+	
+	/**
+	 * shows completion time of each process
+	 * @return
+	 */
+	public static int[] getCompletionTime() {
+		int previousCompletionTime = 0;
+		completionTime[0] = 0;
+		
+		for (int rowIndex = 0; rowIndex < TOTAL_PROCESSES; rowIndex++) {
+			if (previousCompletionTime < arrivalAndBurstTiming[rowIndex][0]) {
+				completionTime[rowIndex] = arrivalAndBurstTiming[rowIndex][0] + arrivalAndBurstTiming[rowIndex][1];
+			} else {
+				completionTime[rowIndex] = previousCompletionTime + arrivalAndBurstTiming[rowIndex][1];
+			}
+			previousCompletionTime = completionTime[rowIndex];
+		}
+		return completionTime;
+	}
+	
+	/**
+	 * shows turn-around time for each process
+	 * @return
+	 */
+	public static int[] getTurnAroundTime() {
+		
+		for (int rowIndex = 0; rowIndex < TOTAL_PROCESSES; rowIndex++) {
+			turnAroundTime[rowIndex] = completionTime[rowIndex] - arrivalAndBurstTiming[rowIndex][0];
+		}
+		
+		return turnAroundTime;
+	}
+	
+	/**
+	 * shows waiting time of each process
+	 * @return
+	 */
+	public static int[] getWaitingTime() {
+		
+		getTurnAroundTime();
+		
+		for (int rowIndex = 0; rowIndex < TOTAL_PROCESSES; rowIndex++) {
+			waitingTime[rowIndex] = turnAroundTime[rowIndex] - arrivalAndBurstTiming[rowIndex][1];
+		}
+		
+		return waitingTime;
+	}
+	
+	/**
+	 * gives average waiting time of all processes
+	 * @return
+	 */
+	public static int getAverageWaitingTime() {
+		int totalWaitingTime = 0, maxWaitingTime = 0;
+		for(int singleWaitingTime : waitingTime) {
+			totalWaitingTime += singleWaitingTime;
+			if (maxWaitingTime < singleWaitingTime)
+				maxWaitingTime = singleWaitingTime;
+		}
+		return (totalWaitingTime/TOTAL_PROCESSES);
+	}
+	
+	/**
+	 * gives total waiting time of all processes
+	 * @return
+	 */
+	public static int getTotalWaitingTime() {
+		int totalWaitingTime = 0, maxWaitingTime = 0;
+		for(int singleWaitingTime : waitingTime) {
+			totalWaitingTime += singleWaitingTime;
+			if (maxWaitingTime < singleWaitingTime)
+				maxWaitingTime = singleWaitingTime;
+		}
+		return totalWaitingTime;
+	}
+	
+	/**
+	 * shows arrival, burst, completion, turn-around and waiting time respectively for all processes
+	 */
+	public static void showProcessesInfo() {
+		System.out.println("A : B : C : T : W");
+		System.out.println("-----------------");
+		for (int rowIndex = 0; rowIndex < TOTAL_PROCESSES; rowIndex++) {
+			for (int colIndex = 0; colIndex < 2; colIndex++) {
+				System.out.print(arrivalAndBurstTiming[rowIndex][colIndex++] + " : "
+								+ arrivalAndBurstTiming[rowIndex][colIndex] + " : "
+								+ completionTime[rowIndex] + " : "
+								+ turnAroundTime[rowIndex] + " : "
+								+ waitingTime[rowIndex] + "\n");
+			}
+		}
+	}
+
+}
