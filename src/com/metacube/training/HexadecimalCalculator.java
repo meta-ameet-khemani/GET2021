1832c1f47988c839b842948de0c3d556f1b29217
Ameet Khemani <ameet.khemani@metacube.com>
2021-02-26 17:49:43 +0530

    removed unwanted files and some modifications in code

--- src/com/metacube/training/HexadecimalCalculator.java
+++ src/com/metacube/training/HexadecimalCalculator.java
@@ -4,10 +4,10 @@ public class HexadecimalCalculator {
 
 	public static void main(String[] args) {
 
-//		System.out.println(HexadecimalArithmeticOperations.hexadecimalToDecimal("AB"));
-//		System.out.println(HexadecimalArithmeticOperations.decimalToHexadecimal(HexadecimalArithmeticOperations.hexadecimalToDecimal("AB")));
-//		System.out.println(HexadecimalArithmeticOperations.add("AB", "10"));
-//		System.out.println(HexadecimalComparisonOperations.areEqual("-1", "tr"));
+		System.out.println(HexadecimalArithmeticOperations.hexadecimalToDecimal("AB"));
+		System.out.println(HexadecimalArithmeticOperations.decimalToHexadecimal(HexadecimalArithmeticOperations.hexadecimalToDecimal("AB")));
+		System.out.println(HexadecimalArithmeticOperations.hexadecimalToDecimal(HexadecimalArithmeticOperations.add("AB", "10")));
+		System.out.println(HexadecimalComparisonOperations.areEqual("-1", "tr"));
 		System.out.println(HexadecimalComparisonOperations.firstLessThanSecond("av", "a"));
 	}
 
