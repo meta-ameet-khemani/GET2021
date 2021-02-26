3850e6174814a8a2ed6cc9beea30a685f8111948
Ameet Khemani <ameet.khemani@metacube.com>
2021-02-26 17:45:31 +0530

    removed unwanted files and some correction in code

--- Assignment1-Shopping-Cart/src/com/metacube/ShoppingCart.java
+++ Assignment1-Shopping-Cart/src/com/metacube/ShoppingCart.java
@@ -1,8 +1,10 @@
 package com.metacube;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
+
 import com.metacube.model.Cart;
 import com.metacube.model.CartItem;
 import com.metacube.model.Item;
@@ -23,31 +25,37 @@ public class ShoppingCart {
 		Scanner input = new Scanner(System.in);
 		int option = 0;
 		showOptions();
-		do {
-			option = input.nextInt();
-			if (option == 1) {
-				showItemsList(itemList);
-				showOptions();
-			} else if (option == 2) {
-				System.out.println("Enter id of item");
-				int itemId = input.nextInt();
-				System.out.println("Enter quantity of item");
-				int quantity = input.nextInt();
-				addItemToCart(itemId, quantity);
-				showCart();
-				showOptions();
-			} else if (option == 3) {
-				showCart();
-				showOptions();
-			} else if (option == 4) {
-				editItem();
-			} else if (option == 0) {
-				System.out.println("Thank you. Visit again");
-			} else {
-				System.out.println("---------- Invalid option ---------");
-				showOptions();
-			}
-		} while (option != 0);
+		try {
+			do {
+				option = input.nextInt();
+				if (option == 1) {
+					showItemsList(itemList);
+					showOptions();
+				} else if (option == 2) {
+					System.out.println("Enter id of item");
+					int itemId = input.nextInt();
+					System.out.println("Enter quantity of item");
+					int quantity = input.nextInt();
+					addItemToCart(itemId, quantity);
+					showCart();
+					showOptions();
+				} else if (option == 3) {
+					showCart();
+					showOptions();
+				} else if (option == 4) {
+					editItem();
+				} else if (option == 0) {
+					System.out.println("Thank you. Visit again");
+				} else {
+					System.out.println("---------- Invalid option ---------");
+					showOptions();
+				}
+			} while (option != 0);
+		} catch (InputMismatchException inputMismatchException) {
+			System.out.println("Invalid input");
+		} catch (Exception exception) {
+			System.out.println(exception.getMessage());
+		}
 		input.close();
 	}
 
@@ -58,25 +66,30 @@ public class ShoppingCart {
 		Scanner input = new Scanner(System.in);
 		// showCart();
 		System.out.println("1 to delete the item, 2 to edit the item, 0 to exit");
-		int option = input.nextInt();
-		if (option == 1) {
-			System.out.println("Enter the id of item to delete");
-			int itemId = input.nextInt();
-			deleteItem(itemId);
-		} else if (option == 2) {
-			System.out.println("Enter the id of item to edit");
-			int itemId = input.nextInt();
-			System.out.println("Enter quantity of item");
-			int quantity = input.nextInt();
-			updateItemInCart(itemId, quantity);
-		} else if (option == 0) {
+		try {
+			int option = input.nextInt();
+			if (option == 1) {
+				System.out.println("Enter the id of item to delete");
+				int itemId = input.nextInt();
+				deleteItem(itemId);
+			} else if (option == 2) {
+				System.out.println("Enter the id of item to edit");
+				int itemId = input.nextInt();
+				System.out.println("Enter quantity of item");
+				int quantity = input.nextInt();
+				updateItemInCart(itemId, quantity);
+			} else if (option == 0) {
+				showOptions();
+			} else {
+				System.out.println("-------- Invalid Option ---------");
+			}
+			
 			showOptions();
-		} else {
-			System.out.println("-------- Invalid Option ---------");
+		} catch (InputMismatchException inputMismatchException) {
+			System.out.println("Invalid input");
+		} catch (Exception exception) {
+			System.out.println(exception.getMessage());
 		}
-		
-		input.close();
-		showOptions();
 	}
 	
 //	private static Item getItemById(int itemId) {
@@ -136,7 +149,7 @@ public class ShoppingCart {
 	 * @param itemId
 	 */
 	private static void deleteItem(int itemId) {
-		int itemIndexToDelete = getIndexOfItemById(itemId);
+		int itemIndexToDelete = getIndexOfCartItemByItemId(itemId);
 		if (itemIndexToDelete == -1) {
 			System.out.println("Invalid item id ...");
 		} else {
@@ -210,15 +223,7 @@ public class ShoppingCart {
 				int previousQuantity = cart.cartItems.get(alreadyPresentCartItemIndex).getQuantity();
 				cart.cartItems.get(alreadyPresentCartItemIndex).setQuantity(previousQuantity + quantity);
 			}
-		}
-		
-		if (itemIndex != -1) {
-			cart.cartItems.get(itemIndex).setQuantity(quantity);
-		} else {
-			Item newItem = itemList.stream().filter(item -> item.getId() == itemId).findFirst().orElse(new Item(itemIndex, null, itemIndex));
-			cart.cartItems.add(new CartItem(newItem, quantity));
-		}
-		
+		}	
 	}
 	
 	/**
