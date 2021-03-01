95e258c33fed4f33dc4d040c88b452614cbbe3cc
Ameet Khemani <ameet.khemani@metacube.com>
2021-03-01 20:30:17 +0530

    changed method compare()

--- src/com/metacube/training/HexadecimalCalculator.java
+++ src/com/metacube/training/HexadecimalCalculator.java
@@ -9,6 +9,9 @@ public class HexadecimalCalculator {
 		System.out.println(HexadecimalArithmeticOperations.hexadecimalToDecimal(HexadecimalArithmeticOperations.add("AB", "10")));
 		System.out.println(HexadecimalComparisonOperations.areEqual("-1", "tr"));
 		System.out.println(HexadecimalComparisonOperations.firstLessThanSecond("av", "a"));
+		
+		System.out.println(HexadecimalArithmeticOperations.isValidHexadecimalNumber("12"));
+		System.out.println(HexadecimalComparisonOperations.compareHexadecimals("dc", "cd"));
 	}
 
 }
